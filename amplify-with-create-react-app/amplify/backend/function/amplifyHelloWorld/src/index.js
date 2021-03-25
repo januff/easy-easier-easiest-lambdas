1cea851462c81cba2787f9cf2a23d8a4e996320d
Joey Anuff <joeyanuff@gmail.com>
2021-03-24 21:41:50 -0700

    images

--- amplify-with-create-react-app/amplify/backend/function/amplifyHelloWorld/src/index.js
+++ amplify-with-create-react-app/amplify/backend/function/amplifyHelloWorld/src/index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
     const response = {
         statusCode: 200,
-        body: JSON.stringify(`Hello from Lambda! Params are ${JSON.stringify(event.queryStringParameters || event)}`),
+        body: JSON.stringify(`Hello from Lambda! Params or event: ${JSON.stringify(event.queryStringParameters || event)}`),
     }
     return response 
 }
